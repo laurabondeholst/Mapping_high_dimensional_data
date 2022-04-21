1de12f584d6c94986eaf20c2f3d122f9135c19d6
s173953 <s173953@studdent.dtu.dk>
2022-04-21 11:23:47 +0200

    kmeans merged with AML

--- src/AML.py
+++ src/AML.py
@@ -13,7 +13,81 @@ from tqdm.auto import tqdm
 import numpy as np
 import warnings
 
-from t_sne_implementation import tsne
+
+##### kemans_clustering
+"""
+This script takes the coordinates of the mapping method (the output from t-SNE in my case)
+as well as the true labels of the correndsponding data and produces a plot of the error rate
+
+TODO: modify the ranges to fit your data
+TODO: modify the filepath and names of the output from the mapping method and true labels
+
+"""
+
+import numpy as np
+from sklearn.cluster import KMeans
+from sklearn.model_selection import train_test_split
+# import matplotlib.pyplot as plt
+import plotly.graph_objects as go
+
+DATA_PATH = "data/processed/noisy_mnist/tsne_results/"
+
+
+def run_kmeans(Y, labels, n_clusters = 2, test_size = 0.5):
+    Y_train, Y_test, labels_train, labels_test = train_test_split(Y, labels, test_size=test_size, random_state=42)
+
+    kmeans = KMeans(
+        init="random",
+        n_clusters=n_clusters,
+        #  n_init=10,
+        max_iter=300,
+        random_state=42
+    )
+
+    kmeans.fit(Y_train)
+
+    y_pred = kmeans.labels_
+
+    # if n_clusters != 2: ## not working: several clusters get assigned same id
+    #     y_pred_new = y_pred
+    #     for cluster in range(n_clusters):
+    #         idx = np.where(y_pred == cluster)
+    #         subset_labels = labels_train[idx]
+    #         subset_labels = np.sort(subset_labels)
+    #         med = np.median(subset_labels)
+
+    #         y_pred_new[idx] = med
+
+    #     y_pred = y_pred_new
+        
+    mistakes_true_labels = 0
+    mistakes_inverted_labels = 0
+    for i in range(len(y_pred)):
+        if y_pred[i] != labels_train[i]:
+            mistakes_true_labels += 1
+        else: 
+            mistakes_inverted_labels += 1
+    
+    # we assume that correct labels have smallest error
+    if mistakes_true_labels < mistakes_inverted_labels: 
+        label_map = {0: 0, 1: 1}
+    else:
+        label_map = {0: 1, 1: 0}
+
+    y_pred_test = kmeans.predict(Y_test)
+
+    mistakes = 0
+    for i in range(len(y_pred_test)):
+        assumed_label = label_map[y_pred_test[i]]
+        if assumed_label != labels_test[i]:
+            mistakes += 1
+        
+    # mistakes = mistakes_e1 if (mistakes_e1 < mistakes_e2) else mistakes_e2
+    return 1 - mistakes/len(y_pred_test)
+
+####
+
+
 from validation import kmeans_clustering as kmeans
 
 import plotly.graph_objects as go
@@ -199,7 +273,7 @@ for ns in noise_range:
       # print(zero/i)
 
       y_pred= pca = PCA(n_components=3).fit_transform(X)
-      correct_count_pca.append(kmeans.run_kmeans(y_pred, y, test_size=0.5))
+      correct_count_pca.append(run_kmeans(y_pred, y, test_size=0.5))
 
       if trimap_enable: 
         if(i<=4):
@@ -229,11 +303,11 @@ for ns in noise_range:
 
       if tsne_enable: 
         y_pred = TSNE(n_components=2, init='pca', random_state=0).fit_transform(X)
-        correct_count_tsne.append(kmeans.run_kmeans(y_pred, y, test_size=1-train_size))
+        correct_count_tsne.append(run_kmeans(y_pred, y, test_size=1-train_size))
 
       if umap_enable: 
         y_pred = umap.UMAP().fit_transform(X)
-        correct_count_umap.append(kmeans.run_kmeans(y_pred, y, test_size=1-train_size))
+        correct_count_umap.append(run_kmeans(y_pred, y, test_size=1-train_size))
 
 
     ns_i_list.append([ns, i])
